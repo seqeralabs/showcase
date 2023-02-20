f53672742586ea4411779a2454c960ce7f1f9680
Harshil Patel <drpatelhh@gmail.com>
2023-02-20 13:58:58 +0000

    Add scipt to import compute envs

---
+++ scripts/tw_computeenvs_import.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+WORKSPACE='seqeralabs/showcase'
+JSON_DIR=../compute-envs
+
+## Overwrite compute environment in Workspace if required
+OVERWRITE=false
+
+## Loop through compute environments and import into Workspace
+JSONS=($JSON_DIR/*.json)
+for i in "${!JSONS[@]}"
+do
+    JSON=${JSONS[$i]}
+    NAME=$(basename "$JSON" .json)
+
+    CREDENTIALS=$(echo $NAME | cut -d "_" -f1,2 )"_credentials"
+    if [[ "$NAME" == *"seqera_aws"* ]]; then
+        CREDENTIALS="seqera_aws_development_credentials"
+    fi
+    if [[ "$NAME" == *"dragen"* ]]; then
+        CREDENTIALS="seqera_aws_management_credentials"
+    fi
+
+    EXISTING_NAMES=( $( tw -o json compute-envs list --workspace=$WORKSPACE | jq -r '.computeEnvs[].name' ) )
+    if [[ " ${EXISTING_NAMES[*]} " =~ " ${NAME} " ]]
+    then
+        if [[ $OVERWRITE != false ]]
+        then
+            echo "[Workspace: '$WORKSPACE'] Deleting Compute Environment: '$NAME'"
+            tw compute-envs delete --workspace=$WORKSPACE --name=$NAME
+            echo "[Workspace: '$WORKSPACE'] Importing Compute Environment: '$NAME'"
+            tw compute-envs import --workspace=$WORKSPACE --name=$NAME --credentials=$CREDENTIALS --wait=AVAILABLE $JSON
+        fi
+    else
+        echo "[Workspace: '$WORKSPACE'] Importing Compute Environment: '$NAME'"
+        tw compute-envs import --workspace=$WORKSPACE --name=$NAME --credentials=$CREDENTIALS --wait=AVAILABLE $JSON
+    fi
+done
